c4ab0f44ad44a3027ab5708e0ae07cc6fe7824ee
Artag/Chris <scristxyz@gmail.com>
2023-11-11 15:22:52 -0500

    primer comentario

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/config/app_theme.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Toktik',
+      debugShowCheckedModeBanner: false,
+      theme: Apptheme().getTheme(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Toktik'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
