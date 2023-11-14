1a40db66f874d19aea4b63bd532eca3e2ad22f5a
Artag/Chris <scristxyz@gmail.com>
2023-11-14 17:23:07 -0500

    botones de videos dinamicos

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_player/video_buttons.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -7,25 +8,22 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      //scroll hacia abajo o arriba
-      scrollDirection: Axis.vertical,
-      //solo para android
-      physics: const BouncingScrollPhysics(),
-      children: [
-        Container(
-          color: Colors.red,
-        ),
-        Container(
-          color: Colors.blue,
-        ),
-        Container(
-          color: Colors.yellow,
-        ),
-        Container(
-          color: Colors.purple,
-        )
-      ],
-    );
+    return PageView.builder(
+        //scroll hacia abajo o arriba
+        scrollDirection: Axis.vertical,
+        //solo para android
+        physics: const BouncingScrollPhysics(),
+        itemCount: videos.length,
+        itemBuilder: (context, index) {
+          final VideoPost videoPost = videos[index];
+
+          return Stack(
+            children: [
+              //video player + gradiente
+              //botones
+            Positioned(bottom: 40, right: 20, child: VideoButtons(video: videoPost)),
+            ],
+          );
+        });
   }
 }
